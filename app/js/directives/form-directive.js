ebc5b0659bb74c6aa4f653048860e04cb1ba399f
praveenjp <roadizpraveen@gmail.com>
2016-05-26 22:23:10 +0530

    form submit to backend

--- app/js/directives/form-directive.js
+++ app/js/directives/form-directive.js
@@ -2,9 +2,20 @@
 
 app.directive('formDirective', function () {
     return {
-        controller: function($scope){
+        controller: function($scope,$http){
             $scope.submit = function(field){
-                console.log(field);
+                
+                console.log(field.form_fields.length);
+                var values = [];
+                for(var i=0; i<field.form_fields.length; i++){
+                    values.push(field.form_fields[i].field_value);
+                }
+                console.log(values);
+
+                $http.post('/form', values).success(function(response){
+                    console.log('Server Response'+response);
+                })
+                
                 $scope.form.submitted = true;
             }
         },

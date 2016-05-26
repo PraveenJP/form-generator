ba43e103bc642c51a535468428d7db9b3dd521f9
praveenjp <roadizpraveen@gmail.com>
2016-05-26 07:46:55 +0530

    node backend updated

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 'use strict'
 var express = require('express');
 var app = express();
+var path = require('path');
 var PORT = process.env.PORT || 3000;
 
 // Path to app directive
-app.use(express.static(__dirname+'/app'));
-// Path to Node modules
-app.use('/node_modules',express.static(__dirname+'/node_modules'));
-// Path to Bower components
-app.use('/bower_components',express.static(__dirname+'/bower_components'));
+app.use(express.static(path.join(__dirname+'/app')));
 
+// Path to Node modules
+app.use('/node_modules',express.static(path.join(__dirname+'/node_modules')));
+
+// Path to Bower components
+app.use('/bower_components',express.static(path.join(__dirname+'/bower_components')));
+
+// Send form
+app.get('/form', function(req,res){
+	console.log(res);
+})
+
+// Listen Server
 app.listen(PORT,function(err){
    if(err){
        console.log(err)
